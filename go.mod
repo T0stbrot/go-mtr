df817fdd6d4b0fe7894ff37ec38d300c44afdcf8
t0stbrot <daniel.wm210@gmail.com>
2025-04-01 18:59:01 +0200

    Initial Release

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/t0stbrot/go-mtr
+
+go 1.24.1
+
+require (
+	github.com/t0stbrot/go-ping v0.0.8 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net/netip"
+	"encoding/json"
+	"net/http"
+	ping "github.com/t0stbrot/go-ping"
+)
+
+type Details struct {
+	IP string `json:"ip"`
+	RTT string `json:"rtt"`
+	ASN int `json:"asn,omitempty"`
+	Hostname string `json:"hostname,omitempty"`
+}
+
+func details(addr string) (details Details) {
+	url := fmt.Sprintf("https://t0stbrot.net/pub-api/ip/%v", addr)
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching from API")
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	var response Details
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("Error decoding API JSON")
+		os.Exit(1)
+	}
+
+	return response
+}
+
+func ipIs(addr string) (is int) {
+	ip, _ := netip.ParseAddr(addr)
+	if ip.Is6() {
+		return 6
+	} else if ip.Is4() {
+		return 4
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("You need to provide a Target Address")
+		os.Exit(2)
+	} else {
+		target := args[1]
+
+		if ipIs(target) == 0 {
+			fmt.Println("You need to provide a Valid Target Address")
+		} else {
+
+			msg := fmt.Sprintf("Starting MTR to %v with 255 maximum hops", target)
+			fmt.Println(msg)
+
+			hops := 0
+				
+			for hops <= 255 {
+				var res ping.PingResult
+				var info Details
+				if ipIs(target) == 6 {
+					res = ping.Ping6(target, hops, 1000)
+				} else {
+					res = ping.Ping4(target, hops, 1000)
+				}
+				hops++
+				if ipIs(res.LastHop) != 0 {
+					info = details(res.LastHop)
+					info.RTT = res.RTT+"ms"
+				} else {
+					info = Details{
+						IP: res.Error,
+						RTT: "Timeout",
+					}
+				}
+				msg = fmt.Sprintf("[%2d] [%10s] [AS%6v] %v [%v]", hops-1, info.RTT, info.ASN, info.IP, info.Hostname)
+				fmt.Println(msg)
+				if res.LastHop == target {
+					break
+					os.Exit(0)
+				}
+			}
+		}
+	}
+
+}
