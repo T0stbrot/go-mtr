22742290520e1ac3a5a5d9d34c8792006d82a7e0
t0stbrot <personal@t0stbrot.net>
2025-05-21 18:00:12 +0200

    fix error

--- ping/main.go
+++ ping/main.go
@@ -102,8 +102,6 @@ func Ping(ver int, destination string, ttl int, timeout int, seq int) PingResult
 	if err != nil {
 		result.Message = fmt.Sprintf("%v", err)
 	}
-	if echoReply, ok := reply.Body.(*icmp.Echo); ok {
-    }
 
 	result.LastHop = addr.String()
 	switch reply.Type {
